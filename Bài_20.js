32b19e85ba7e8041cf59f5cbccfba3edc9d010dc
DangggKhoaaa <khoaduong.27011998@gmail.com>
2023-04-07 16:00:54 +0700

    update

--- Bài_20.js
+++ Bài_20.js
@@ -1,9 +1,18 @@
 function biggestLine(arr){
     let sum = 0;
-    let max;
+    let max = 0;
     for(let i = 0; i < arr.length; i++){
         for(let j = 0; j < arr[i].length; j++){
             sum += arr[i][j];
         }
+        if(sum > max){
+            max = sum;
+        }
+        sum = 0;
     }
+    return max;
 }
+console.log(biggestLine([[2,1,5,3],
+                        [1,3,5,7],
+                        [2,4,6,8],
+                        [9,4,-3,7]]));
